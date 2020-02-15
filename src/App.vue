e288ccba8f5506856eee31ca705c43f0a0dbe9d7
wyj <wangyjhfut@163.com>
2020-02-15 20:08:31 +0800

    add files 2019/02/15

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
+      <el-button type="primary">HELLO MIKKY</el-button>
     </div>
     <router-view/>
   </div>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 导入组件库
+import ElementUI from 'element-ui'
+// 导入组件相关样式
+import 'element-ui/lib/theme-chalk/index.css'
+// 配置 Vue 插件
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
